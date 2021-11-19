02480e978bd9f17dd92c991a9c4ec326d6adde89
Peter Swinkels <peter_swinkels@hotmail.com>
2021-11-19 18:33:08 +0100

    Update 8086.vb
    The emulated amount of memory should now be one megabyte instead of 64kb.

--- 8086.vb
+++ 8086.vb
@@ -412,7 +412,7 @@ Public Class CPU8086Class
    Private Const OVERFLOW_TRAP As Integer = &H4%          'Defines the overflow trap interrupt number. 
    Public Const INVALID_OPCODE As Integer = &H6%         'Defines the invalid opcode interrupt number.
 
-   Public Memory() As Byte = Enumerable.Repeat(CByte(&H0%), &H10000%).ToArray()   'Contains the memory used by the emulated 8086 CPU.
+   Public Memory() As Byte = Enumerable.Repeat(CByte(&H0%), &H100000%).ToArray()   'Contains the memory used by the emulated 8086 CPU.
 
    Public Event Halt()                                                                   'Defines the halt event.
    Public Event Interrupt(Number As Integer, AH As Integer)                              'Defines the interrupt event.
